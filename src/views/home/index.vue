41f2ef1f2f5cb6a0b791ce35a08e86412ef49a9f
peris <964480899@qq.com>
2023-08-13 21:59:50 +0800

    init project takeaway-app

---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<!--  -->
+<template>
+  <div>HomeView</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {}
+  },
+
+  components: {}
+}
+</script>
+<style lang="scss" scoped></style>
